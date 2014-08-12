ec2ae9019b0e96d4b824b33c824488bf8cc50d50
pfullen <peter@fullencpa.com>
2014-08-12 12:28:50 -0400

    Added clone and autocomplete
    Added clone for product and Qty  plus autocomple ui for customer

--- createSO.php
+++ createSO.php
@@ -5,24 +5,44 @@ if (php_sapi_name() != 'cli') echo '<pre>'.PHP_EOL;
 // change db name and credentials in this file
 require_once('./db_config.php');
 
-// good habit to get into.
-// never trust user input, always filter post fields
-
-// $Name = filter_var($_POST['Name'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_LOW);
-// $Age = filter_var($_POST['AGE'], FILTER_VALIDATE_INT);
-
-// these work wherever (web, cli)
-$Customer = $_POST["customer"] ;
-$Product =  $_POST["product"];
-$Quantity = $_POST["qty"];
-$DriverID = "2345";
-
-$fields = array('customer' => $Customer, 'product' => $Product, 'quantity' => $Quantity, 'driverId' => $DriverID,
-'salesOrderDate' => date('Y-m-d H:i:s'));
 
 
-// this is a little helper function I use all the time
+
+$customer = $_POST["customer"];
+$notes = $_POST["notes"];
+$lineItem =$_POST["lineItem"];
+$driverid = "27"; 
+
+
+// add to salesOrder Table
+$fields = array( 'customer' => $customer,   'notes' =>  $notes, 'salesOrderDate' => date('Y-m-d H:i:s'), 'driverid' => $driverid);
+
 $sql = $cMySQL->createInsert('salesorders', $fields);
 echo 'SQL: '.$sql.PHP_EOL;
 $insertID = $cMySQL->execQ($sql, $fields);
 echo 'Record ID: '.$insertID.PHP_EOL;
+
+
+$salesOrderID = $insertID;
+
+// now add to lineItem table
+
+
+foreach ($lineItem as $key) {
+
+$product = $key["product"];
+$qty = $key["qty"];
+$price = "27.95";
+print_r($key["product"]. " ");
+print_r($key["qty"]. " ");
+
+$fields = array ('product' => $product, 'qty' => $qty, 'price'=> $price,  'salesOrderId' => $salesOrderID);
+$sql = $cMySQL->createInsert('lineitems', $fields);
+echo 'SQL: '.$sql.PHP_EOL;
+$insertID = $cMySQL->execQ($sql, ($fields));
+echo 'Record ID: '.$insertID.PHP_EOL;
+}
+
+
+
+?>
